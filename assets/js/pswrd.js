5d6a81c8816c925e588ed113b71752d668bd2db4
EVK <eric@ericvankirk.com>
2022-06-14 18:37:59 -0500

    initial password genrator

--- assets/js/pswrd.js
+++ assets/js/pswrd.js
@@ -11,7 +11,26 @@ function writePassword() {
 
   passwordText.value = password;
 
+  
 }
+// var password = document.getElementById("generate");
+function generatePassword(){
+  // returns a password
+ var chars = "0123456789";
+ var passwordLength = 12;
+ var password = "";
+
+ for (var i = 0; i <= passwordLength; i++){
+var randomNumber = Math.floor(Math.random() * chars.length);
+password += chars.substring(randomNumber, randomNumber +1);
+
+document.querySelector("#generate").value = password;
+
+ }
+  
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
