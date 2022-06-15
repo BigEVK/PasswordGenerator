f22ce85c2a939cf83f6ee957854fb729f93585b7
EVK <eric@ericvankirk.com>
2022-06-14 22:16:05 -0500

    add special chars

--- assets/js/pswrd.js
+++ assets/js/pswrd.js
@@ -7,27 +7,25 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  console.log(password);
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-  
+  passwordText.value = password;  
 }
+
+
 // var password = document.getElementById("generate");
 function generatePassword(){
   // returns a password
- var chars = "0123456789";
+ var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmopqrstuvwxyk!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
  var passwordLength = 12;
  var password = "";
 
  for (var i = 0; i <= passwordLength; i++){
 var randomNumber = Math.floor(Math.random() * chars.length);
 password += chars.substring(randomNumber, randomNumber +1);
-
-document.querySelector("#generate").value = password;
-
  }
-  
+ return password
 }
 
 
